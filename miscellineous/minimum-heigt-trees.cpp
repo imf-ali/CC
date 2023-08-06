d56dbf722b2889e1128a5047b75dbf0e3a3234b7
Fahad Ali <alifahad2210@gmail.com>
2023-08-06 12:50:08 +0530

    minimum-height-trees

---
+++ miscellineous/minimum-heigt-trees.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
+        vector<vector<int>> graph(n);
+        vector<int>indegree(n,0);
+        for(auto it : edges){
+            graph[it[0]].push_back(it[1]);
+            graph[it[1]].push_back(it[0]);
+            indegree[it[0]]++;
+            indegree[it[1]]++;
+        }
+        queue<int> q;
+        for(int i=0;i<n;i++){
+            if(indegree[i] == 1)
+                q.push(i);
+        }
+        vector<int>res;
+        while(!q.empty()){
+            int sz = q.size();
+            res.clear();
+            while(sz--){
+                int node = q.front();
+                q.pop();
+                res.push_back(node);
+                for(auto it : graph[node]){
+                    if(--indegree[it] == 1)
+                        q.push(it);
+                }
+            }
+        }
+        return res;
+    }
+};
